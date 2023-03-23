9628add9d799e37cf3495d756f3fc0169c11e89b
Dilpreet <92905896+dfordp@users.noreply.github.com>
2023-03-23 11:51:51 +0530

    Final Commit

---
+++ week-3-remaining-programs/DFS.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+import time
+ 
+class Graph:
+ 
+
+    def __init__(self):
+
+        self.graph = defaultdict(list)
+ 
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+ 
+    def DFSUtil(self, v, visited):
+ 
+        visited.add(v)
+        print(v, end=' ')
+ 
+        for neighbour in self.graph[v]:
+            if neighbour not in visited:
+                self.DFSUtil(neighbour, visited)
+
+    def DFS(self, v):
+        visited = set()
+        self.DFSUtil(v, visited)
+ 
+if __name__ == "__main__":
+    g = Graph()
+    g.addEdge(0, 1)
+    g.addEdge(0, 2)
+    g.addEdge(1, 2)
+    g.addEdge(2, 0)
+    g.addEdge(2, 3)
+    g.addEdge(3, 3)
+ 
+    print("Following is DFS from (starting from vertex 2)")
+    start=time.time()
+    g.DFS(2)
+    end=time.time()
+    print((end-start)*10**3,"ms")
---
+++ week-3-remaining-programs/JobSequencingwtihDealinesWhileMaxProfits.py
@@ -0,0 +1,33 @@
+import time
+
+def printJobSequence(arr,t):
+    n=len(arr)
+
+    #for sorting in decreasing order of profit
+    for i in range(n):
+        for j in range(n-1-i):
+            if arr[j][2]< arr[j+1][2]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    
+    result=[False]*t
+    job=['1']*t
+
+    for i in range(n):
+        for j in range(min(t-1,arr[i][1]-1),-1,-1):
+            if result[j] is False:
+                result[j]=True
+                job[j]=arr[i][0]
+                break
+    print(job)
+
+arr=[['a',2,100],
+     ['b',1,19],
+     ['c',2,27],
+     ['d',1,25],
+     ['e',3,15]]
+
+print("Following is maximum Profit Job Sequencing:")
+start=time.time()
+printJobSequence(arr,3)
+end=time.time()
+print((end-start)*10**3,"ms")
